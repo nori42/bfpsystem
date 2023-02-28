327e1672be89dc3c9aefda1f97ddc7ab7d466d2d
chasauce19 <chasauce19@gmail.com>
2023-02-28 22:50:11 +0800

    Panel Update

--- app/Http/Controllers/EstablishmentController.php
+++ app/Http/Controllers/EstablishmentController.php
@@ -74,11 +74,6 @@ class EstablishmentController extends Controller
         ->select('establishments.*', 'owners.*')
         ->where('record_no', (int)request('id'))
         ->first();
-<<<<<<< HEAD
-=======
-
->>>>>>> a467887109ef8e0589f82b67e9d9a0212ed39a1c
-
         return view('establishments.show', ['establishment' => $establishment]);
     }
 
